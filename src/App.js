9e0380bedd21721fe15f6b472a64105969618056
marcuses101 <mnjconnolly@gmail.com>
2020-09-26 14:51:38 -0400

    initial layout complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import List from "./compositions/List";
 
-function App() {
+function App(props) {
+  const { store } = props;
+  const listsJSX = store.lists.map(list=>{
+    return (
+      <List
+      key={list.id}
+      header={list.header}
+      cards={list.cardIds.map(id=> store.allCards[id])}
+      />
+    )
+  })
+
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trelloyes!</h1>
       </header>
-    </div>
+      <div className="App-list">
+        {listsJSX}
+      </div>
+    </main>
   );
 }
 
---
+++ src/compositions/List.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Card";
+import "./List.css";
+
+function List(props) {
+  const { header, cards } = props;
+
+  const cardsJSX = cards.map((card, index) => (
+    <Card title={card.title} content={card.content} key={card.id} />
+  ));
+
+  return (
+    <section className="List">
+      <header className="List-header">
+        <h2>{header}</h2>
+      </header>
+      <div className="List-cards">
+        {cardsJSX}
+        <button type="button" className="List-add-button">
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default List;
